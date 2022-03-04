c6286b2ffa739af801edcbe51e59b191ef221008
Geoffrey Eslava <geoffrey.eslava@gmail.com>
2022-03-04 13:04:11 +0800

    Fixed the components for adding new Projects (without validation).

--- src/projects/NewProjectForm.tsx
+++ src/projects/NewProjectForm.tsx
@@ -1,46 +1,110 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Project } from "./Project";
 
+interface NewProjectFormProps {
+  project: Project;
+  onCancel: () => void;
+  onSave: (project: Project) => void;
+}
 
-function handleSubmit() {}
+function NewProjectForm(props: NewProjectFormProps) {
+  const [errors, setErrors] = useState({
+    name: "",
+    description: "",
+    budget: "",
+  });
+  const [projName, setProjName] = useState("");
+  const [projDescription, setProjDescription] = useState("");
+  const [projBudget, setProjBudget] = useState("");
+  const [projImageUrl, setProjImageUrl] = useState("");
+  const [projIsActive, setProjIsActive] = useState("");
 
-function handleChange(){}
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    props.project.name = projName;
+    props.project.description = projDescription;
+    props.project.budget = Number(projBudget);
+    props.project.imageUrl = projImageUrl;
+    props.project.isActive = Boolean(projIsActive);
+    props.project.contractTypeId = Math.floor(Math.random() * 10) + 1;
 
-function onCancel(){}
+    props.onSave(props.project);
+  };
 
-const NewProjectForm = () => {
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
       <label htmlFor="name">Project Name</label>
-      <input type="text" name="name" placeholder="enter name" onChange={handleChange}/>
-      {/*{errors.name.length > 0 && (*/}
-      {/*  <div className="card error">*/}
-      {/*    <p>{errors.name}</p>*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      <input
+        type="text"
+        name="name"
+        placeholder="enter name"
+        onChange={(event) => {
+          setProjName(event.target.value);
+        }}
+      />
+      {errors.name.length > 0 && (
+        <div className="card error">
+          <p>{errors.name}</p>
+        </div>
+      )}
       <label htmlFor="description">Project Description</label>
-      <textarea name="description" placeholder="enter description" onChange={handleChange}/>
-      {/*{errors.description.length > 0 && (*/}
-      {/*  <div className="card error">*/}
-      {/*    <p>{errors.description}</p>*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      <textarea
+        name="description"
+        placeholder="enter description"
+        onChange={(event) => {
+          setProjDescription(event.target.value);
+        }}
+      />
+      {errors.description.length > 0 && (
+        <div className="card error">
+          <p>{errors.description}</p>
+        </div>
+      )}
       <label htmlFor="budget">Project Budget</label>
-      <input type="number" name="budget" placeholder="enter budget" onChange={handleChange}/>
-      {/*{errors.budget.length > 0 && (*/}
-      {/*  <div className="card error">*/}
-      {/*    <p>{errors.budget}</p>*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      <input
+        type="number"
+        name="budget"
+        placeholder="enter budget"
+        onChange={(event) => {
+          setProjBudget(event.target.value);
+        }}
+      />
+      <label htmlFor="imageUrl">Image URL</label>
+      <input
+        type="text"
+        name="imageUrl"
+        placeholder="enter image url"
+        onChange={(event) => {
+          setProjImageUrl(event.target.value);
+        }}
+      />
+      {errors.budget.length > 0 && (
+        <div className="card error">
+          <p>{errors.budget}</p>
+        </div>
+      )}
       <label htmlFor="isActive">Active?</label>
-      <input type="checkbox" name="isActive" onChange={handleChange}/>
+      <input
+        type="checkbox"
+        name="isActive"
+        onChange={(event) => {
+          setProjIsActive(event.target.value);
+        }}
+      />
 
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
-        <span/>
-        <button type="button" className="bordered medium" onClick={onCancel}>cancel</button>
+        <span />
+        <button
+          type="button"
+          className="bordered medium"
+          onClick={props.onCancel}
+        >
+          Reset
+        </button>
       </div>
     </form>
   );
-};
+}
 
 export default NewProjectForm;
--- src/projects/NewProjectPage.tsx
+++ src/projects/NewProjectPage.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import { addDoc, collection } from "firebase/firestore";
+import React from "react";
 import NewProjectForm from "./NewProjectForm";
+import { Project } from "./Project";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const NewProjectPage = () => {
+  const newProject = new Project();
+  const projectCollectionRef = collection(db, "projects");
+  const navigate = useNavigate();
+
+  function saveProject(project: Project) {
+    //create async function that saves the data
+    // to firebase.
+    const createProject = async () => {
+      await addDoc(projectCollectionRef, {
+        name: project.name,
+        budget: project.budget,
+        contractSignedOn: project.contractSignedOn,
+        contractTypeId: project.contractTypeId,
+        description: project.description,
+        imageUrl: project.imageUrl,
+        isActive: project.isActive,
+      });
+    };
+
+    // navigate away from the current page.
+    createProject().then(() => {
+      return navigate("/");
+    });
+  }
+
+  function resetFields() {
+    /*
+    Clear all the values of the input field in the page.
+     */
+    Array.from(document.querySelectorAll("input")).forEach((input) => {
+      input.value = "";
+      input.checked = false;
+    });
+
+    Array.from(document.querySelectorAll("textarea")).forEach((textarea) => {
+      textarea.value = "";
+    });
+  }
   return (
     <div>
       New Project Page
-      <NewProjectForm/>
+      <NewProjectForm
+        project={newProject}
+        onSave={saveProject}
+        onCancel={resetFields}
+      />
     </div>
   );
 };
