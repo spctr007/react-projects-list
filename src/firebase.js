ecb365e350d31ca97ff6f11c9a2a7f6d631df4be
Geoffrey Eslava <geoffrey.eslava@gmail.com>
2022-03-04 12:59:14 +0800

    Added firebase (firestore) db backend.

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBc-WZNZ8zYilA_v7qij6vmF-1tcyJDpRM",
+    authDomain: "react-projects-list.firebaseapp.com",
+    databaseURL: "https://react-projects-list-default-rtdb.firebaseio.com",
+    projectId: "react-projects-list",
+    storageBucket: "react-projects-list.appspot.com",
+    messagingSenderId: "169905327088",
+    appId: "1:169905327088:web:c9714b379af0b29f22ab35",
+    measurementId: "G-KQ09PHL4KZ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
