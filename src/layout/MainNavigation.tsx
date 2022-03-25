b5c4853ce8609ce5bef5b479ea32a6fe5065e161
Geoffrey Eslava <geoffrey.eslava@gmail.com>
2022-03-25 11:40:52 +0800

    Removed unused imports.

--- src/layout/MainNavigation.tsx
+++ src/layout/MainNavigation.tsx
@@ -1,16 +1,17 @@
-import {Link} from "react-router-dom";
 import React from "react";
-import navStyles from './MainNavigation.module.css';
 
 export default function MainNavigation() {
-
   return (
     <header className="masthead">
-        <div className="container">
-            <div className="masthead-subheading">Welcome To Our Studio!</div>
-            <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-            <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+      <div className="container">
+        <div className="masthead-subheading">Welcome To Our Studio!</div>
+        <div className="masthead-heading text-uppercase">
+          It's Nice To Meet You
         </div>
+        <a className="btn btn-primary btn-xl text-uppercase" href="#services">
+          Tell Me More
+        </a>
+      </div>
     </header>
   );
 }
